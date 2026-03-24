a47389283e75ff3729542388b061b5327a255695
BabayevAsad <esed.babayev2002@gmail.com>
2026-03-24 22:10:58 +0100

    Initialize Spring Boot project

---
+++ src/main/java/com/AsadBabayev/sportradar_sports_calendar/SportradarSportsCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.AsadBabayev.sportradar_sports_calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportradarSportsCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportradarSportsCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AsadBabayev/sportradar_sports_calendar/SportradarSportsCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AsadBabayev.sportradar_sports_calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportradarSportsCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
