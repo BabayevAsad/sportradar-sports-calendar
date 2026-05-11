a0a3667f9048869790c569455ba5ff9f6fce4503
BabayevAsad <esed.babayev2002@gmail.com>
2026-05-11 15:34:48 +0200

    feat(cache): add Redis configuration and CacheManager

---
+++ src/main/java/com/AsadBabayev/sportradar_sports_calendar/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.AsadBabayev.sportradar_sports_calendar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory){
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(5))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new JacksonJsonRedisSerializer<Object>(Object.class)));
+
+        return RedisCacheManager
+                .builder(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+}
